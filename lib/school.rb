0a05173871c055f1a072f6a5e0a130fbe180fb54
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-08 06:10:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,12 +13,13 @@ def initialize(name)
     roster[level] << student
   end 
   
-    def grade(level)
-    roster.detect do |x, y| 
-      if x == level
-        return y 
-      end 
+    def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
   end 
+  nu_hash
+end 
 end 
 
 school = School.new("Bayside High School")
