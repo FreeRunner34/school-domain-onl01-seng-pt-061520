5a75b4330988535af078701819279ee4be65e8ae
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-08 06:25:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,30 @@
-class School 
-attr_accessor :name, :roster
+class School
 
-def initialize(name)
-  @name = name 
-  @roster = {}
-  end 
-  
-    def add_student(student, level)
-    roster[level] ||= []
-    roster[level] << student
-  end 
-  
-    def sort 
-      nu_hash = {}
-      roster.each do |x, y| 
-      nu_hash[x] = y.sort 
-    end 
-    nu_hash
-  end 
-end 
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
 
+  def roster
+    @roster
+  end
 
-school = School.new("Bayside High School")
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
